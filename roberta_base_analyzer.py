3828373d4a60a8a74eb8025078ec3c8db67ae61f
Tianchu Ji <tianchu.ji@stonybrook.edu>
2020-11-16 03:16:08 -0500

    add sparsity evaluation

--- roberta_base_analyzer.py
+++ roberta_base_analyzer.py
@@ -40,34 +40,31 @@ def convert_att_to_np(x, attn_mask):
 
 def convert_hist_to_np(x): return np.asarray([layer.cpu().numpy() for layer in x])
 
-def run_mask_fill_pipeline(model_name: str, num_sentences):
+def run_mask_fill_pipeline(model_name: str, inst: str, masked_idx: int, att_threshold=0.0, hs_threshold=0.0, head_mask=None):
     mf_pipeline = pipeline("fill-mask", model=model_name, tokenizer=model_name, device=0, \
-        topk=20, att_threshold=0.0001, output_attentions=True, output_hidden_states=True)
+        topk=20, att_threshold=att_threshold, hs_threshold=hs_threshold, \
+        output_attentions=True, output_hidden_states=True)
 
     # fetch data:
-    insts, masked_insts, correct_token = extract_inst_wikipedia(num_sentences), [], []
-    for inst in insts:
-        word_list = inst.split(' ')
-        rand_token_to_mask = random.randint(0, len(word_list)-1)
-        correct_token.append(word_list[rand_token_to_mask])
-        word_list[rand_token_to_mask] = '[MASK]'
-        masked_insts.append(" ".join(word_list))
+    masked_insts, correct_token = [], []
 
-    results = mf_pipeline(masked_insts)
+    word_list = inst.split(' ')
+    correct_token = word_list[masked_idx]
+    word_list[masked_idx] = '[MASK]'
+    masked_inst = " ".join(word_list)
+
+    results = mf_pipeline([masked_inst])
     
-    correct = 0
-    for res_idx, res in enumerate(results['result']):
-        for possible_res in res:
-            if possible_res['token_str'].lower() == correct_token[res_idx].lower():
-                correct += 1
-                break
+    correctness = False
+    for possible_res in results['result'][0]:
+        if possible_res['token_str'].lower() == correct_token.lower():
+            correctness = True
+            break
 
-    print("em: ", float(correct)/num_sentences)
-
-    return float(correct)/num_sentences, results['hidden_states'], results['attentions']
+    return correctness, results['hidden_states'], results['attentions']
     
 
-def get_atten_hist_from_model(model_name: str, num_sentences: int):
+def get_atten_hist_from_model(model_name: str, num_sentences: int, att_threshold=0.0, hs_threshold=0.0):
     param_file_path = PARAM_PATH + model_name
 
     attentions, attn_mask, hists, sparse_hist = None, None, None, None
@@ -116,6 +113,81 @@ def get_atten_hist_from_model(model_name: str, num_sentences: int):
     print ("Shape of attention weight matrices", len(attentions), attentions[0].shape)
     return attentions, hists
 
+def get_em_sparsity_from_masked_lm(model_name: str, num_sentences: int, att_threshold=0.0, hs_threshold=0.0):
+    
+    total_score, inst_count, all_max, all_min, all_mean, all_std, all_sparsity = \
+        None, None, None, None, None, None, None
+    
+    # read from file
+    input_type = "_all"
+    score_path, att_stat_path = (PARAM_PATH + i + input_type + '.npy' \
+                                    for i in ['score', 'att_stat_features'])
+    if os.path.isfile(score_path) and os.path.isfile(att_stat_path):
+        print("Loading parameters from file {}...".format(PARAM_PATH + input_type))
+        with open(score_path, "rb") as score_file:
+            total_score, inst_count = (i for i in np.load(score_file))
+        with open(att_stat_path, "rb") as att_stat_file:
+            all_max = np.load(att_stat_file)
+            all_min = np.load(att_stat_file)
+            all_mean = np.load(att_stat_file)
+            all_std = np.load(att_stat_file)
+            all_sparsity = np.load(att_stat_file)
+    # extract parameters from model
+    else:
+        insts = extract_inst_wikipedia(num_sentences)
+        random.seed(13312)
+        masked_ids = [random.randint(0, len(i.split(' '))-1) for i in insts]
+
+        head_mask=None
+
+        glb_counter, correct_counter = 0, 0
+        res, total_elem_count = None, 0
+        for inst, masked_id in zip(insts, masked_ids):
+            print("running pipeline iter {}/{}".format(glb_counter+1, num_sentences))
+            correctness, hidden_states, attentions = \
+                run_mask_fill_pipeline(model_name, inst, masked_id, \
+                    att_threshold=att_threshold, hs_threshold=hs_threshold, head_mask=head_mask)
+            if correctness: correct_counter += 1
+            glb_counter += 1
+            em_score = float(correct_counter) / glb_counter
+
+            def get_spars(x, axis): 
+                return x.shape[-1] ** 2 - np.count_nonzero(x[:, :, :, :], axis=axis)
+            def agg_func(f): return np.stack([f(i, axis=(-2, -1)) for i in attentions], axis=0)
+            def add_func(f): return np.sum([f(i, axis=(-2, -1)) for i in attentions], axis=0)
+            if res is None:
+                res = {'score': em_score, 'mean': agg_func(np.mean),
+                    'max': agg_func(np.amax), 'min': agg_func(np.amin), 
+                    'std': agg_func(np.std), 'sparsity': add_func(get_spars)}
+            else:
+                res['score'] = em_score
+                res['max'] = np.concatenate((res['max'], agg_func(np.amax)), axis=0)
+                res['min'] = np.concatenate((res['min'], agg_func(np.amin)), axis=0)
+                res['mean'] = np.concatenate((res['mean'], agg_func(np.mean)), axis=0)
+                res['std'] = np.concatenate((res['std'], agg_func(np.std)), axis=0)
+                res['sparsity'] = np.add(res['sparsity'], add_func(get_spars))
+
+            total_elem_count += sum([att.shape[-1] * att.shape[-1] for att in attentions])
+
+        res['sparsity'] = res['sparsity'].astype(float) / total_elem_count
+
+        # save params
+        total_score, inst_count, all_max, all_min, all_mean, all_std, all_sparsity = \
+            res['score'], num_sentences, res['max'], res['min'], res['mean'], res['std'], res['sparsity']
+
+        with open(score_path, "wb+") as scores_file:
+            np.save(scores_file, np.array([total_score, inst_count]))
+        with open(att_stat_path, "wb+") as att_stat_file:
+            np.save(att_stat_file, all_max)
+            np.save(att_stat_file, all_min)
+            np.save(att_stat_file, all_mean)
+            np.save(att_stat_file, all_std)
+            np.save(att_stat_file, all_sparsity)
+    
+    print("total score: ", total_score, "#sentences: ", inst_count,
+          "max dim:", all_max.shape, "min dim:", all_min.shape,
+          "mean dim:", all_mean.shape, "std dim:", all_std.shape,
+          "sparsity dim:", all_sparsity.shape)
 
 def get_sparse_hist_token(attn, offset, sparsity_bar=0.0):
     all_sparse_count = None
@@ -231,18 +303,37 @@ def list_sparse_tokens_all(model_name, sparsity_bar=0.0, num_sentences=500):
 
 
 if __name__ == "__main__":
+    arg_parser = ag.ArgumentParser(description=__doc__)
+    arg_parser.add_argument("-at", "--att_threshold", default=0.0,
+                            required=False, help="set attention sparsity threshold")
+    arg_parser.add_argument("-ht", "--hs_threshold", default=0.0,
+                            required=False, help="set hidden states sparsity threshold")
+    arg_parser.add_argument("-d", "--distribution", default=False, action='store_true',
+                            required=False, help="print histogram")
+    arg_parser.add_argument("-e", "--evaluation", default=False, action="store_true",
+                            required=False, help="evaluate model only without any plot")
+    arg_parser.add_argument("-sa", "--samples", default=-1,
+                            required=False, help="number of samples for distribution")
+
+    args = vars(arg_parser.parse_args())
+    att_threshold = float(args['att_threshold'])
+    hs_threshold = float(args['hs_threshold'])
+    samples = int(args['samples'])
+
     # list_sparse_tokens_all("roberta-base", sparsity_bar=1e-8, num_sentences=8000)
+    
+    if args['evaluation']:
+        get_em_sparsity_from_masked_lm("bert-base-uncased", samples, att_threshold=att_threshold, hs_threshold=hs_threshold)
 
-    run_mask_fill_pipeline('bert-base-uncased', 5)
-    exit()
+    if args['distribution']:
+        attns, hists = get_atten_hist_from_model('bert-base-uncased', samples, att_threshold=att_threshold, hs_threshold=hs_threshold)
+        attn_mask = [i.shape[-1] for i in attns]
+        print(hists.shape, len(attn_mask))
 
-    attns, hists = get_atten_hist_from_model('bert-base-uncased', 3)
-    attn_mask = [i.shape[-1] for i in attns]
-    print(hists.shape, len(attn_mask))
+        # h_state sanity check
+        for i in range(10):
+            print("h_state mean:{:.4f}, std:{:.4f}".format(
+                np.mean(hists[0][0][i*5], axis=-1), np.std(hists[0][0][i*5], axis=-1)))
 
-    # h_state sanity check
-    for i in range(10):
-        print("h_state mean:{:.4f}, std:{:.4f}".format(
-            np.mean(hists[0][0][i*5], axis=-1), np.std(hists[0][0][i*5], axis=-1)))
-    # tv.plot_atten_dist_per_token(attns, 100, sparse_hist=get_sparse_hist_token(attns, 0.0))
-    # tv.plot_hs_dist_per_token(hists, 100, attn_mask, scale='linear')
+        tv.plot_atten_dist_per_token(attns, 100, sparse_hist=get_sparse_hist_token(attns, 0.0))
+        tv.plot_hs_dist_per_token(hists, 100, attn_mask, scale='linear')
